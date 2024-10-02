24d7823c8146fed1fd89e64306a094d3a1d2b74d
i-am-santosh17 <santoshpalande45@gmail.com>
2024-10-02 11:51:44 +0530

    adding local project to git

---
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.PackMan;
+import com.in28minutes.learn_spring_framework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+    public static void main(String[] args){
+        MarioGame marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var packMan = new PackMan();
+        GameRunner gameRunner = new GameRunner(packMan);
+        gameRunner.run();
+    }
+}
