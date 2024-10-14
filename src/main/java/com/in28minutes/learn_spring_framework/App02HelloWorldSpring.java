15778357bef643087e65cdf324c86fdc213ffdcc
i-am-santosh17 <santoshpalande45@gmail.com>
2024-10-14 23:01:49 +0530

    Autowired bean using parameters

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -13,7 +13,9 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("myCustomAddress"));
+        System.out.println(context.getBean("person2Parameter"));
+        System.out.println(context.getBean(Address.class));
 
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name,int age){};
+record Person(String name,int age,Address address){};
 record Address(String firstLine,String city){};
 @Configuration
 public class HelloWorldConfiguration {
@@ -19,10 +19,16 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person(){
-        return new Person("Steve",23);
+        return new Person("Steve",23,new Address("Baker street","Pune"));
     }
-    @Bean Address address(){
+    @Bean(name = "myCustomAddress")
+    Address address(){
         return new Address("Baker street","Pune");
+    }
+
+    @Bean
+    Person person2Parameter(String name,int age,Address myCustomAddress){
+        return new Person(name,age,myCustomAddress);
     }
 
 }
